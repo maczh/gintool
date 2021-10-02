4d38c3b93e80fc0c4fbeccfebc571f41452bbdba
maczh <maczh@qq.com>
2021-10-03 01:13:42 +0800

    修改

--- postlog.go
+++ postlog.go
@@ -2,21 +2,19 @@ package gintool
 
 import (
 	"gopkg.in/mgo.v2/bson"
-	"ququ.im/common"
 )
 
 type PostLog struct {
-	ID            bson.ObjectId          `bson:"_id"`
-	Time          string                 `json:"time" bson:"time"`
-	RequestId     string                 `json:"requestId" bson:"requestId"`
-	Responsetime  string                 `json:"responsetime" bson:"responsetime"`
-	TTL           int                    `json:"ttl" bson:"ttl"`
-	Apiname       string                 `json:"apiName" bson:"apiName"`
-	Controller    string                 `json:"controller" bson:"controller"`
-	Tradenum      string                 `json:"tradenum" bson:"tradenum"`
-	Accountid     string                 `json:"accountid" bson:"accountid"`
-	Requestparam  map[string]string      `json:"requestparam" bson:"requestparam"`
-	Responseparam common.Result          `json:"responseparam" bson:"responseparam"`
-	Responsestr   string                 `json:"responsestr" bson:"responsestr"`
-	Responsemap   map[string]interface{} `json:"responsemap" bson:"responsemap"`
+	ID           bson.ObjectId          `bson:"_id"`
+	Time         string                 `json:"time" bson:"time"`
+	RequestId    string                 `json:"requestId" bson:"requestId"`
+	Responsetime string                 `json:"responsetime" bson:"responsetime"`
+	TTL          int                    `json:"ttl" bson:"ttl"`
+	Apiname      string                 `json:"apiName" bson:"apiName"`
+	Controller   string                 `json:"controller" bson:"controller"`
+	Tradenum     string                 `json:"tradenum" bson:"tradenum"`
+	Accountid    string                 `json:"accountid" bson:"accountid"`
+	Requestparam map[string]string      `json:"requestparam" bson:"requestparam"`
+	Responsestr  string                 `json:"responsestr" bson:"responsestr"`
+	Responsemap  map[string]interface{} `json:"responsemap" bson:"responsemap"`
 }
